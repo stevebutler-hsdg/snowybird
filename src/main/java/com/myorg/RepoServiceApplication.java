b895ac489224312bc5bb42aa4459e7141ce99d41
stevebutler-hsdg <stephen.butler@hamburgsud.com>
2017-01-10 11:34:14 +0100

    Initial commit from the atomist bot

---
+++ src/main/java/com/myorg/RepoServiceApplication.java
@@ -0,0 +1,12 @@
+package com.myorg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoServiceApplication.class, args);
+    }
+}
